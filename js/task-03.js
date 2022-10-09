e83ef4a3e3e0eb469ae1a93ed3acef2319026094
VitaliiTymko <intehmet2015@gmail.com>
2022-10-09 13:58:21 +0300

    Task-03 finish

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,26 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const newArrayRef = document.querySelector(".gallery");
+const galleryRef = [];
+
+for (let i=0; i<images.length; i+=1) {
+  const liRef = document.createElement("li");
+  const imageRef = document.createElement("img");
+  imageRef.src = images[i].url;  
+  imageRef.alt = images[i].alt;
+  imageRef.width = 150;
+  liRef.classList.add("item-task-03");
+  liRef.append(imageRef);
+  // console.log(liRef);
+  galleryRef.push(liRef);
+  // console.log(galleryRef);
+}
+newArrayRef.append(...galleryRef);
+
+console.log(newArrayRef);
+// Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
