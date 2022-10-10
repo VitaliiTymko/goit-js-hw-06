a15a5f699167f0c06fe57e42bc361b8d754773fb
VitaliiTymko <intehmet2015@gmail.com>
2022-10-11 01:08:48 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const colorRef = document.querySelector(".color");
+const bodyRef = document.querySelector("body");
+
+changeColorButton.addEventListener("click", (event) => {
+  const randomColor = (bodyRef.style.backgroundColor = getRandomHexColor());
+  
+  colorRef.textContent = randomColor.value;
+});
